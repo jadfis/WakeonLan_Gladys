9b3526cfb796beaa997c28923b9b77bd30414179
jadfis <kurzojul11@hotmail.fr>
2018-07-16 21:20:43 +0200

    0.0.1

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ module.exports = function(sails) {
 
       var wol = require('wake-on-lan');
 
-      wol('A0:D3:C1:4B:79:BB', {
-        ip: '192.168.1.75',
+      wol('A8:D3:F7:4C:21:6C', {
+        ip: '192.168.1.121',
         port: 9
       }, function(error) {
         if(error) {
